733ce11ef6a8961358fbabc6f0d14ce842d7414c
ekrasutzky <ekrasutzky@gmail.com>
2023-06-28 15:26:27 +0000

    add new style and a restart game button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,96 @@
-let buttons = document.querySelectorAll('.btn');
-let result = document.getElementById('result');
+const quizContainer = document.getElementById('quiz');
+const scoreContainer = document.getElementById('score-value');
+const feedbackContainer = document.getElementById('feedback');
+const startButton = document.getElementById('start-btn');
 
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    let playerSelection = button.id;
-    let computerSelection = computerPlay();
-    let gameResult = playRound(playerSelection, computerSelection);
-    result.textContent = gameResult;
-  });
-});
+const questions = [
+    {
+        question: "Germany is the largest country in Europe.",
+        answer: false,
+        image: "germany.jpg"
+    },
+    {
+        question: "The capital city of Germany is Berlin.",
+        answer: true,
+        image: "berlin.jpg"
+    },
+    {
+        question: "German is the official language of Germany.",
+        answer: true,
+        image: "german.jpg"
+    },
+    // Add more questions here
+];
 
-function computerPlay() {
-  let choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
-  let randomIndex = Math.floor(Math.random() * choices.length);
-  return choices[randomIndex];
+let score = 0;
+let currentQuestion = 0;
+
+startButton.addEventListener('click', startQuiz);
+
+function startQuiz() {
+    startButton.style.display = 'none';
+    renderQuestion();
 }
 
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    return "It's a tie!";
-  } else if (
-    (playerSelection === 'rock' && (computerSelection === 'scissors' || computerSelection === 'lizard')) || 
-    (playerSelection === 'paper' && (computerSelection === 'rock' || computerSelection === 'spock')) ||
-    (playerSelection === 'scissors' && (computerSelection === 'paper' || computerSelection === 'lizard')) ||
-    (playerSelection === 'lizard' && (computerSelection === 'paper' || computerSelection === 'spock')) ||
-    (playerSelection === 'spock' && (computerSelection === 'rock' || computerSelection === 'scissors'))
-  ) {
-    return `You win! ${playerSelection} beats ${computerSelection}.`;
-  } else {
-    return `You lose! ${computerSelection} beats ${playerSelection}.`;
-  }
+function renderQuestion() {
+    quizContainer.innerHTML = '';
+
+    if (currentQuestion >= questions.length) {
+        finishQuiz();
+        return;
+    }
+
+    const question = questions[currentQuestion];
+    const questionElement = document.createElement('div');
+    questionElement.innerHTML = question.question;
+    quizContainer.appendChild(questionElement);
+
+    const trueButton = createButton('True');
+    const falseButton = createButton('False');
+
+    trueButton.addEventListener('click', () => {
+        checkAnswer(true, question);
+    });
+    falseButton.addEventListener('click', () => {
+        checkAnswer(false, question);
+    });
+
+    quizContainer.appendChild(trueButton);
+    quizContainer.appendChild(falseButton);
+}
+
+function createButton(text) {
+    const button = document.createElement('button');
+    button.innerHTML = text;
+    return button;
+}
+
+function checkAnswer(userAnswer, question) {
+    if (userAnswer === question.answer) {
+        score++;
+        feedbackContainer.innerHTML = '<span class="correct">Correct!</span>';
+    } else {
+        feedbackContainer.innerHTML = '<span class="incorrect">Incorrect!</span>';
+    }
+
+    scoreContainer.innerHTML = score;
+    currentQuestion++;
+    renderQuestion();
+}
+
+function finishQuiz() {
+    quizContainer.innerHTML = 'Congratulations! You have completed the quiz.';
+    feedbackContainer.innerHTML = '';
+
+    startButton.style.display = 'inline-block';
+    startButton.innerHTML = 'Restart';
+    startButton.addEventListener('click', restartQuiz);
+}
+
+function restartQuiz() {
+    score = 0;
+    currentQuestion = 0;
+    scoreContainer.innerHTML = score;
+    feedbackContainer.innerHTML = '';
+    startQuiz();
 }
